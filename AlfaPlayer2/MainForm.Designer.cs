78433b63053cebe48a43871d587ac0180f848a65
Ionut Oancea <ionut.oancea@onecom.no>
2016-04-16 00:57:13 +0300

    3

--- AlfaPlayer2/MainForm.Designer.cs
+++ AlfaPlayer2/MainForm.Designer.cs
@@ -28,26 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textProgressBar3 = new AlfaPlayer.TextProgressBar();
+            this.labelSongTitle = new System.Windows.Forms.Label();
+            this.labelBatteryInfo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textProgressBar = new AlfaPlayer.TextProgressBar();
             this.textProgressBar2 = new AlfaPlayer.TextProgressBar();
-            this.cListBox1 = new AlfaPlayer.CListBox();
+            this.listBoxFilePanel = new AlfaPlayer.CListBox();
             this.textProgressBar1 = new AlfaPlayer.TextProgressBar();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textProgressBar3
+            // labelSongTitle
             // 
-            this.textProgressBar3.DataBindings.Add(new System.Windows.Forms.Binding("TextColor", global::AlfaPlayer2.Properties.Settings.Default, "ProgressTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.textProgressBar3.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "ProgressForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.textProgressBar3.ForeColor = global::AlfaPlayer2.Properties.Settings.Default.ProgressForeColor;
-            this.textProgressBar3.Location = new System.Drawing.Point(20, 16);
-            this.textProgressBar3.Margin = new System.Windows.Forms.Padding(11, 7, 11, 7);
-            this.textProgressBar3.MaximumValue = 100;
-            this.textProgressBar3.Name = "textProgressBar3";
-            this.textProgressBar3.ProgressText = "3566";
-            this.textProgressBar3.Size = new System.Drawing.Size(445, 62);
-            this.textProgressBar3.TabIndex = 3;
-            this.textProgressBar3.TextColor = global::AlfaPlayer2.Properties.Settings.Default.ProgressTextColor;
-            this.textProgressBar3.Value = 44;
+            this.labelSongTitle.AutoSize = true;
+            this.labelSongTitle.BackColor = global::AlfaPlayer2.Properties.Settings.Default.TitleBackColor;
+            this.labelSongTitle.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "TitleTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.labelSongTitle.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AlfaPlayer2.Properties.Settings.Default, "TitleBackColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.labelSongTitle.ForeColor = global::AlfaPlayer2.Properties.Settings.Default.TitleTextColor;
+            this.labelSongTitle.Location = new System.Drawing.Point(55, 123);
+            this.labelSongTitle.Name = "labelSongTitle";
+            this.labelSongTitle.Size = new System.Drawing.Size(267, 31);
+            this.labelSongTitle.TabIndex = 4;
+            this.labelSongTitle.Text = "Alfa Player";
+            // 
+            // labelBatteryInfo
+            // 
+            this.labelBatteryInfo.AutoSize = true;
+            this.labelBatteryInfo.BackColor = global::AlfaPlayer2.Properties.Settings.Default.OnBatteryColor;
+            this.labelBatteryInfo.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AlfaPlayer2.Properties.Settings.Default, "OnBatteryColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.labelBatteryInfo.Location = new System.Drawing.Point(531, 100);
+            this.labelBatteryInfo.Name = "labelBatteryInfo";
+            this.labelBatteryInfo.Size = new System.Drawing.Size(83, 31);
+            this.labelBatteryInfo.TabIndex = 5;
+            this.labelBatteryInfo.Text = "100";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxFilePanel);
+            this.groupBox1.Location = new System.Drawing.Point(41, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(752, 364);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textProgressBar
+            // 
+            this.textProgressBar.DataBindings.Add(new System.Windows.Forms.Binding("TextColor", global::AlfaPlayer2.Properties.Settings.Default, "ProgressTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textProgressBar.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "ProgressForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.textProgressBar.ForeColor = global::AlfaPlayer2.Properties.Settings.Default.ProgressForeColor;
+            this.textProgressBar.Location = new System.Drawing.Point(20, 16);
+            this.textProgressBar.Margin = new System.Windows.Forms.Padding(11, 7, 11, 7);
+            this.textProgressBar.MaximumValue = 100;
+            this.textProgressBar.Name = "textProgressBar";
+            this.textProgressBar.ProgressText = "3566";
+            this.textProgressBar.Size = new System.Drawing.Size(445, 62);
+            this.textProgressBar.TabIndex = 3;
+            this.textProgressBar.TextColor = global::AlfaPlayer2.Properties.Settings.Default.ProgressTextColor;
+            this.textProgressBar.Value = 44;
             // 
             // textProgressBar2
             // 
@@ -61,25 +99,27 @@
             this.textProgressBar2.TextColor = System.Drawing.Color.Empty;
             this.textProgressBar2.Value = 0;
             // 
-            // cListBox1
+            // listBoxFilePanel
             // 
-            this.cListBox1.BackColor = global::AlfaPlayer2.Properties.Settings.Default.BackgroundColor;
-            this.cListBox1.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AlfaPlayer2.Properties.Settings.Default, "BackgroundColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cListBox1.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "FilePanelTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cListBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedItemBackColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedItemBackColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cListBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedItemForeColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedItemForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cListBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedSpecialItemForeColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedSpecialItemForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.cListBox1.ForeColor = global::AlfaPlayer2.Properties.Settings.Default.FilePanelTextColor;
-            this.cListBox1.FormattingEnabled = true;
-            this.cListBox1.ItemHeight = 31;
-            this.cListBox1.Location = new System.Drawing.Point(20, 213);
-            this.cListBox1.Name = "cListBox1";
-            this.cListBox1.SelectedItemBackColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedItemBackColor;
-            this.cListBox1.SelectedItemForeColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedItemForeColor;
-            this.cListBox1.SelectedSpecialItemForeColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedSpecialItemForeColor;
-            this.cListBox1.ShowScrollbar = false;
-            this.cListBox1.Size = new System.Drawing.Size(495, 314);
-            this.cListBox1.TabIndex = 1;
+            this.listBoxFilePanel.BackColor = global::AlfaPlayer2.Properties.Settings.Default.BackgroundColor;
+            this.listBoxFilePanel.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AlfaPlayer2.Properties.Settings.Default, "BackgroundColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.listBoxFilePanel.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "FilePanelTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.listBoxFilePanel.DataBindings.Add(new System.Windows.Forms.Binding("SelectedItemBackColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedItemBackColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.listBoxFilePanel.DataBindings.Add(new System.Windows.Forms.Binding("SelectedItemForeColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedItemForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.listBoxFilePanel.DataBindings.Add(new System.Windows.Forms.Binding("SelectedSpecialItemForeColor", global::AlfaPlayer2.Properties.Settings.Default, "SelectedSpecialItemForeColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.listBoxFilePanel.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBoxFilePanel.ForeColor = global::AlfaPlayer2.Properties.Settings.Default.FilePanelTextColor;
+            this.listBoxFilePanel.FormattingEnabled = true;
+            this.listBoxFilePanel.ItemHeight = 67;
+            this.listBoxFilePanel.Location = new System.Drawing.Point(20, 96);
+            this.listBoxFilePanel.Name = "listBoxFilePanel";
+            this.listBoxFilePanel.SelectedItemBackColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedItemBackColor;
+            this.listBoxFilePanel.SelectedItemForeColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedItemForeColor;
+            this.listBoxFilePanel.SelectedSpecialItemForeColor = global::AlfaPlayer2.Properties.Settings.Default.SelectedSpecialItemForeColor;
+            this.listBoxFilePanel.ShowScrollbar = false;
+            this.listBoxFilePanel.Size = new System.Drawing.Size(622, 180);
+            this.listBoxFilePanel.TabIndex = 1;
+            this.listBoxFilePanel.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBoxFilePanel_DrawItem);
             // 
             // textProgressBar1
             // 
@@ -105,9 +145,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = global::AlfaPlayer2.Properties.Settings.Default.BackgroundColor;
             this.ClientSize = new System.Drawing.Size(800, 600);
-            this.Controls.Add(this.textProgressBar3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelBatteryInfo);
+            this.Controls.Add(this.labelSongTitle);
+            this.Controls.Add(this.textProgressBar);
             this.Controls.Add(this.textProgressBar2);
-            this.Controls.Add(this.cListBox1);
             this.Controls.Add(this.textProgressBar1);
             this.DataBindings.Add(new System.Windows.Forms.Binding("BackColor", global::AlfaPlayer2.Properties.Settings.Default, "BackgroundColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.DataBindings.Add(new System.Windows.Forms.Binding("ForeColor", global::AlfaPlayer2.Properties.Settings.Default, "TitleTextColor", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
@@ -118,16 +160,22 @@
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private AlfaPlayer.TextProgressBar textProgressBar1;
-        private AlfaPlayer.CListBox cListBox1;
+        private AlfaPlayer.CListBox listBoxFilePanel;
         private AlfaPlayer.TextProgressBar textProgressBar2;
-        private AlfaPlayer.TextProgressBar textProgressBar3;
+        private AlfaPlayer.TextProgressBar textProgressBar;
+        private System.Windows.Forms.Label labelSongTitle;
+        private System.Windows.Forms.Label labelBatteryInfo;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- AlfaPlayer2/MainForm.cs
+++ AlfaPlayer2/MainForm.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NAudio.Wave;
+using NAudio.WindowsMediaFormat;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -15,6 +19,308 @@ namespace AlfaPlayer2
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void listBoxFilePanel_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+            Rectangle r = e.Bounds;
+            r.Offset(0, (int)((e.Bounds.Height - e.Font.Size) / 2));
+            r.Width *= 10;
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                e = new DrawItemEventArgs(e.Graphics,
+                                          e.Font,
+                                          e.Bounds,
+                                          e.Index,
+                                          e.State ^ DrawItemState.Selected,
+                                          e.ForeColor,
+                                          listBoxFilePanel.SelectedItemBackColor);//Choose the color
+
+            e.DrawBackground();
+
+            Brush b = listBoxFilePanel.selectedItemForeBrush;
+
+            var sss = listBoxFilePanel.Items[e.Index].ToString();
+            if (sss.StartsWith(":"))
+            {
+                sss = "[" + sss.Substring(1) + "]";
+                b = listBoxFilePanel.selectedSpecialItemForeBrush;
+            }
+            e.Graphics.DrawString(sss, e.Font, b, r, StringFormat.GenericDefault);
+
+
+        }
+
+        private byte[] FLAC_HEADER = new byte[] { 0x66, 0x4C, 0x61, 0x43, 0x00, 0x00, 0x00, 0x22 };
+        private byte[] MP3_HEADER = new byte[] { 0x49, 0x44, 0x33 };
+        private byte[] WMA_HEADER = new byte[] { 0x30, 0x26, 0xB2, 0x75, 0x8E, 0x66, 0xCF, 0x11, 0xA6, 0xD9, 0x00, 0xAA, 0x00, 0x62, 0xCE, 0x6C };
+
+        private enum FileType
+        {
+            None,
+            FLAC,
+            MP3,
+            WMA
+        }
+
+        private FileType DetectFileType(string fn)
+        {
+            FileType ft = FileType.None;
+            var rs = File.OpenRead(fn);
+            byte[] buf = new byte[16];
+            rs.Read(buf, 0, 16);
+            if (Enumerable.SequenceEqual(buf.Take(16), WMA_HEADER))
+            {
+                ft = FileType.WMA;
+            }
+            else if (Enumerable.SequenceEqual(buf.Take(8), FLAC_HEADER))
+            {
+                ft = FileType.FLAC;
+            }
+            else if (Enumerable.SequenceEqual(buf.Take(3), MP3_HEADER))
+            {
+                ft = FileType.MP3;
+            }
+
+            if (ft == FileType.None)
+            {
+                switch (Path.GetExtension(fn).ToLowerInvariant())
+                {
+                    case ".mp3":
+                        ft = FileType.MP3;
+                        break;
+
+                    case ".flac":
+                        ft = FileType.FLAC;
+                        break;
+
+                    case ".wma":
+                        ft = FileType.WMA;
+                        break;
+                }
+            }
+
+            return ft;
+        }
+
+
+
+        private void PlayNext()
+        {
+            if (listBoxFilePanel.SelectedIndex < listBoxFilePanel.Items.Count - 1)
+            {
+                listBoxFilePanel.SelectedIndex++;
+                OpenFile(groupBox1.Tag.ToString() + Path.DirectorySeparatorChar + listBoxFilePanel.SelectedItem.ToString());
+            }
+        }
+
+        private void PlayPrev()
+        {
+            if (listBoxFilePanel.SelectedIndex > 0)
+            {
+                listBoxFilePanel.SelectedIndex--;
+            }
+            OpenFile(groupBox1.Tag.ToString() + Path.DirectorySeparatorChar + listBoxFilePanel.SelectedItem.ToString());
+        }
+
+        private int scrollPos = 0;
+
+
+        private WaveStream reader;
+        private TagLib.File tag;
+        private WaveOut waveOut;
+
+        private void OpenFile(string fn, double pos = 0)
+        {
+            Console.WriteLine(fn);
+
+            if (!File.Exists(fn) || Directory.Exists(fn))
+            {
+                return;
+            }
+
+            Console.WriteLine("{0}  {1}", fn, DetectFileType(fn));
+
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+            }
+            if (reader != null)
+            {
+                reader.Close();
+                reader.Dispose();
+            }
+
+            var ft = DetectFileType(fn);
+            if (ft == FileType.None)
+            {
+                return;
+            }
+
+            tag = TagLib.File.Create(fn);
+            try
+            {
+                switch (ft)
+                {
+                    case FileType.MP3:
+                        reader = new Mp3FileReader(fn);
+                        break;
+
+                    case FileType.FLAC:
+                        reader = new NAudio.Flac.FlacReader(fn);
+                        break;
+
+                    case FileType.WMA:
+                        reader = new WMAFileReader(fn);
+                        break;
+                }
+                reader.CurrentTime = TimeSpan.FromSeconds(pos);
+                lastFile = Path.GetFullPath(fn);
+                listBoxFilePanel.SelectedItem = Path.GetFileName(fn);
+                Console.WriteLine("reader.BlockAlign {0}", reader.BlockAlign);
+                if (ft != FileType.None)
+                {
+                    scrollPos = 0;
+                    waveOut = new WaveOut(); // or WaveOutEvent()
+                    waveOut.Init(reader);
+                    waveOut.Play();
+                    SaveSettings();
+                }
+            }
+            catch { }
+        }
+        private float maxVolume = 0.5f;
+
+        private DateTime lastSeekForward;
+
+        private void SeekForward()
+        {
+            if (reader != null)
+            {
+                try
+                {
+                    double s = 0.0005 * (100 + Math.Max(900, (DateTime.Now - lastSeekForward).TotalMilliseconds));
+                    waveOut.Volume = 0.1f * maxVolume;
+                    reader.CurrentTime += TimeSpan.FromSeconds(1 / s);
+                }
+                catch (Exception) { }
+            }
+            lastSeekForward = DateTime.Now;
+        }
+
+        private DateTime lastSeekBackward;
+
+        private void SeekBackward()
+        {
+            if (reader != null)
+            {
+                try
+                {
+                    double s = 0.0005 * (100 + Math.Max(900, (DateTime.Now - lastSeekBackward).TotalMilliseconds));
+                    waveOut.Volume = 0.1f * maxVolume;
+                    reader.CurrentTime += TimeSpan.FromSeconds(-1 / s);
+                }
+                catch (Exception) { }
+            }
+            lastSeekBackward = DateTime.Now;
+        }
+
+        private void SeekToSecond(double seconds)
+        {
+            if (reader != null)
+            {
+                try
+                {
+                    waveOut.Volume = 0.1f * maxVolume;
+                    reader.CurrentTime = TimeSpan.FromSeconds(seconds);
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private string lastFolder = @"F:\DATA\MUSIC\www.s-a-f.ro\music\";
+        private string lastFile = @"F:\DATA\MUSIC\www.s-a-f.ro\music\07-the_smoke-upm.mp3";
+        private double lastFilePos = 0;
+
+
+        void SaveSettings()
+        {
+            Console.WriteLine("SAVING");
+            Properties.Settings.Default.LastFile = lastFile;
+            Properties.Settings.Default.LastFilePos = lastFilePos;
+            Properties.Settings.Default.Save();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            labelBatteryInfo.BackColor = Properties.Settings.Default.BackgroundColor;
+            //Properties.Settings.Default.Upgrade();
+            lastFolder = Properties.Settings.Default.LastFolder;
+            lastFile = Properties.Settings.Default.LastFile;
+            lastFilePos = Properties.Settings.Default.LastFilePos;
+            maxVolume = Properties.Settings.Default.MaxVolume;
+
+            InitFilePanel();
+            ChangeFolder(Path.GetDirectoryName(lastFile));
+            OpenFile(lastFile, lastFilePos);
+            SeekToSecond(lastFilePos);
+            listBoxFilePanel.Select();
+        }
+
+
+        private void InitFilePanel()
+        {
+            if (File.Exists(lastFile))
+            {
+                ChangeFolder(Path.GetDirectoryName(lastFile));
+            }
+            else if (Directory.Exists(lastFolder))
+            {
+                ChangeFolder(lastFolder);
+            }
+            else
+            {
+                ChangeFolder(".");
+            }
+        }
+
+        private void ChangeFolder(string folder)
+        {
+            try
+            {
+                folder = Path.GetFullPath(folder);
+                Console.WriteLine("Change folder -> {0}", folder);
+                List<string> dirs = new List<string>(Directory.GetDirectories(folder).Select(f => { f = ":" + f.Substring(f.LastIndexOf(Path.DirectorySeparatorChar) + 1); return f; }).ToArray());
+                string[] extensions = { ".mp3", ".flac", ".wma" };
+
+                List<string> files = new List<string>(Directory.GetFiles(folder, "*.*")
+                    .Where(f => extensions.Contains(System.IO.Path.GetExtension(f).ToLower())).Select(f => { f = f.Substring(f.LastIndexOf(Path.DirectorySeparatorChar) + 1); return f; }).ToArray());
+
+                dirs.Insert(0, @":..");
+                dirs.AddRange(files);
+
+                listBoxFilePanel.DataSource = dirs;
+
+                SetFolderLabel(folder);
+                Properties.Settings.Default.LastFolder = folder;
+                Properties.Settings.Default.Save();
+            }
+            catch (Exception) { }
+        }
+
+        private void SetFolderLabel(string folder)
+        {
+            groupBox1.Tag = folder;
+            int max = 50;
+            if (folder.Length > max)
+            {
+                int dif = folder.Length - max;
+                Console.WriteLine("{0}  {1} {2}", folder.Length, max, dif);
+                folder = folder.Substring(0, folder.Length / 2 - dif / 2) + "..." + folder.Substring(folder.Length / 2 + dif / 2);
+            }
+
+            groupBox1.Text = folder;
         }
     }
 }
